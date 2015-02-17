84bb3e9fd04fa6f2a87cee98c20fd1003976e513
Tim Kelty <tim@fusionary.com>
2015-02-17 15:33:45 -0500

    Refactor

--- tasks/assets/pull.js
+++ tasks/assets/pull.js
@@ -5,36 +5,13 @@ var path = require('path');
 var Promise = require('bluebird');
 var mkdirp = require('mkdirp');
 var chalk = require('chalk');
+var transfer = require('../../lib/transfer');
 
 module.exports = function (gruntOrShipit) {
-  registerTask(gruntOrShipit, 'assets:pull', task);
+  var shipit = getShipit(gruntOrShipit);
+  shipit = transfer(shipit);
 
-  function task() {
-    var shipit = getShipit(gruntOrShipit);
-    var paths = shipit.config.assets.paths;
-    var localPaths = shipit.config.assets.localPaths || paths;
-    shipit.currentPath = path.join(shipit.config.deployTo, 'current');
-    shipit.sharedPath = path.join(shipit.config.deployTo, 'shared');
-    shipit.config.assets.remoteBasePath = shipit.config.assets.remoteBasePath || shipit.sharedPath;
-    shipit.config.assets.localBasePath = shipit.config.assets.localBasePath || process.cwd();
-
-    paths = paths.map(function(value, index) {
-      var remotePath = path.join(shipit.config.assets.remoteBasePath, value);
-      var localPath = path.join(shipit.config.assets.localBasePath, localPaths[index]);
-
-      // Test if dir, add trailing slash to src
-      remotePath = path.join(remotePath, '/');
-
-      // Throw exception is localPaths[index] is undef
-
-      return shipit.remoteCopy(remotePath, localPath, {
-        direction: 'remoteToLocal'
-      });
-    });
-
-    return Promise.all(paths)
-    .then(function() {
-      shipit.log(chalk.green('Assets synced: %s → local'), shipit.environment);
-    });
-  }
+  registerTask(gruntOrShipit, 'assets:pull', function() {
+    return shipit.transfer('remoteToLocal');
+  });
 };
--- tasks/assets/push.js
+++ tasks/assets/push.js
@@ -5,36 +5,13 @@ var path = require('path');
 var Promise = require('bluebird');
 var mkdirp = require('mkdirp');
 var chalk = require('chalk');
+var transfer = require('../../lib/transfer');
 
 module.exports = function (gruntOrShipit) {
-  registerTask(gruntOrShipit, 'assets:push', task);
+  var shipit = getShipit(gruntOrShipit);
+  shipit = transfer(shipit);
 
-  function task() {
-    var shipit = getShipit(gruntOrShipit);
-    var paths = shipit.config.assets.paths;
-    var localPaths = shipit.config.assets.localPaths || paths;
-    shipit.currentPath = path.join(shipit.config.deployTo, 'current');
-    shipit.sharedPath = path.join(shipit.config.deployTo, 'shared');
-    shipit.config.assets.remoteBasePath = shipit.config.assets.remoteBasePath || shipit.sharedPath;
-    shipit.config.assets.localBasePath = shipit.config.assets.localBasePath || process.cwd();
-
-    paths = paths.map(function(value, index) {
-      var remotePath = path.join(shipit.config.assets.remoteBasePath, value);
-      var localPath = path.join(shipit.config.assets.localBasePath, localPaths[index]);
-
-      // Test if dir, add trailing slash to src
-      localPath = path.join(localPath, '/');
-
-      // Throw exception is localPaths[index] is undef
-
-      return shipit.remoteCopy(localPath, remotePath, {
-        direction: 'localToRemote'
-      });
-    });
-
-    return Promise.all(paths)
-    .then(function() {
-      shipit.log(chalk.green('Assets synced: local → %s'), shipit.environment);
-    });
-  }
+  registerTask(gruntOrShipit, 'assets:push', function() {
+    return shipit.transfer('localToRemote');
+  });
 };
